b8ee800b70964fe21b5eba5c6d1a5d3b171b0068
Dimas Triana <dimastriana03@gmail.com>
2025-07-07 17:30:57 +0700

    feat: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-"use client";
-import Image from "next/image";
-import { useTranslation } from "react-i18next";
-// import i18next from "i18next";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-  const { t } = useTranslation();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <h1>{t("slogan")}</h1>
-      </main>
-    </div>
+    <>
+      <Footer />
+    </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#111111] min-h-[200px] px-[15px] py-[20px] lg:px-[60px] lg:py-[30px]">
+      <div className="flex flex-col gap-5 lg:gap-2">
+        <div className="flex gap-2 items-center">
+          <Image src={"/icons/Harun.svg"} width={50} height={50} alt="Harun" />
+          <h1 className="text-3xl font-bold text-white">Harun</h1>
+        </div>
+        <div className="flex flex-col gap-5 lg:flex-row lg:justify-between pb-4 border-b border-[#B2B8C7]">
+          <Link
+            href={"mailto:harunbahrudin59@gmail.com"}
+            className="text-base text-white"
+          >
+            Harunbahrudin59@gmail.com
+          </Link>
+          <div className="flex gap-4">
+            <Link href={"#Home"} className="text-base text-white">
+              Home
+            </Link>
+            <Link href={"#About"} className="text-base text-white">
+              About
+            </Link>
+            <Link href={"#Work"} className="text-base text-white">
+              Work
+            </Link>
+            <Link href={"#Contact"} className="text-base text-white">
+              Contact
+            </Link>
+          </div>
+        </div>
+        <h2 className="text-[#B2B8C7]">All rights reserved © 2025</h2>
+      </div>
+    </footer>
+  );
+}
