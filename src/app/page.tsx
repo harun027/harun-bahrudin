b78953024f495a5d1244b4ac9ab6e5c402367c6b
Dimas Triana <dimastriana03@gmail.com>
2025-07-07 21:56:22 +0700

    Resolve merge conflict in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Contact from "@/components/Contact";
 import FAQs from "@/components/FAQs";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <>
       <Header />
+      <Contact />
       <FAQs />
       <Footer />
     </>
